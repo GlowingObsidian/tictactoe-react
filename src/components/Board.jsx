604bdebec59a79c83fd10e7c0d4d70c40e16cfd4
Shreyan Dey <estrellaproductions@teamfuture.in>
2023-08-22 23:15:58 +0530

    testing

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,6 +4,8 @@ import Square from "./Square";
 const Board = () => {
   const [squares, setSquare] = useState(Array(9).fill(null));
 
+  const hello = null;
+
   const handleSquareClick = (clickedPosition) => {
     setSquare((currentSquares) => {
       return currentSquares.map((squareVal, position) => {
